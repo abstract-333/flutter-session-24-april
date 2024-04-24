8dee5d09cf980c428db8b9dd29785feb64c861ca
bashar <basharhasan74@gmail.com>
2024-04-24 16:12:05 +0300

    add filles

---
+++ flutter_first_example/lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+List<Color> listOfColors = [
+  Colors.red,
+  Colors.cyan,
+  Colors.green,
+  Colors.amber,
+  Colors.grey,
+];
---
+++ flutter_second_example/lib/word.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Word {
+  final String arabicName;
+  final String englishName;
+  final IconData icon;
+
+  Word(
+      {required this.arabicName,
+      required this.englishName,
+      required this.icon});
+}
